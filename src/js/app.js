8c3ca25552846d47ccc2197844af37a2cf986540
Jacob Garcia <rock2494@hotmail.com>
2017-07-04 16:27:40 -0500

    Changed to testing HOST

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ require('angular-scroll-animate')
 
 //Change in production
 //to http://wetopia.co and in development to http://localhost:8080
-window.HOST = 'http://wetopia.co'
+window.HOST = 'http://beautiful-mount-rainier-23260.herokuapp.com/'
 
 angular.module('musementApp', ['ui.router', 'LocalStorageModule',
         'angular-jwt', 'pascalprecht.translate', 'ngFileUpload', 'ngTagsInput', 'ngAnimate', 'chart.js', 'angular-click-outside', 'duScroll','angulartics', 'angulartics.segment','angular-scroll-animate'
